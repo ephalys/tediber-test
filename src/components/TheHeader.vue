ef0506c11ee9b72207baed3cb2d25482f793081d
Léon Tran-Van <ephalys@live.fr>
2023-04-04 19:58:11 +0200

    feat: add TheHeader component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<!--Using functional component here for faster rendering-->
+<template functional>
+  <header id="header">
+    This is a simulated header with the exact same height than the original.
+  </header>
+</template>
+
+<style lang="scss">
+#header {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  min-height: 130px; //make sure that the text is not cropped in mobile view
+  background-color: $primary;
+  color: white;
+  padding: 2rem;
+}
+</style>
