55da0ea0a9a32f040897a7517305464d89df75d0
Léon Tran-Van <ephalys@live.fr>
2023-04-11 12:28:06 +0200

    refactor: add helpers/color.js file to improve readability of OptionSelector

---
+++ src/helpers/colors.js
@@ -0,0 +1,28 @@
+export const COLORS = [
+  { code: "sofa-simple-color_ECRU", value: "#C2B280", isPureColor: true },
+  { code: "sofa-simple-color_GRIS", value: "#61605f", isPureColor: true },
+  { code: "sofa-simple-color_BLEU", value: "#273347", isPureColor: true },
+  { code: "sofa-simple-color_VERT", value: "#689d71", isPureColor: true },
+  { code: "sofa-simple-color_KAKI", value: "#94812b", isPureColor: true },
+  { code: "sofa-simple-color_TERRACOTTA", value: "#d7a285", isPureColor: true },
+  {
+    code: "sofa-simple-feet-color_CLAIR",
+    value: `url(${require("@/assets/img/sofa-simple-feet-color_CLAIR.png")}) center`,
+    isPureColor: false,
+  },
+  {
+    code: "sofa-simple-feet-color_FONCE",
+    value: `url(${require("@/assets/img/sofa-simple-feet-color_FONCE.png")}) center`,
+    isPureColor: false,
+  },
+  {
+    code: "sofa-simple-feet-form_ROND",
+    value: `url(${require("@/assets/img/feet-rond.jpeg")}) center/cover no-repeat`,
+    isPureColor: false,
+  },
+  {
+    code: "sofa-simple-feet-form_CARRE",
+    value: `url(${require("@/assets/img/feet-carre.jpeg")}) center/cover no-repeat`,
+    isPureColor: false,
+  },
+];
